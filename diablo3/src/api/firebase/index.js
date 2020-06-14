59c9e1703205b086c5dc3e784627ed261778fd84
Andrés Rivera <43735082+circuandante@users.noreply.github.com>
2020-06-13 22:16:28 -0500

    aliminar auth

--- diablo3/src/api/firebase/index.js
+++ diablo3/src/api/firebase/index.js
@@ -1,13 +1,1 @@
-// import "firebase/auth";
-// import "firebase/firestore";
 
-export const firebaseConfig = {
-  apiKey: "AIzaSyDf4MB96OsYOMf4Cn6fpoOc8hhY_QlA5zY",
-  authDomain: "practice-3c2f5.firebaseapp.com",
-  databaseURL: "https://practice-3c2f5.firebaseio.com",
-  projectId: "practice-3c2f5",
-  storageBucket: "practice-3c2f5.appspot.com",
-  messagingSenderId: "991096957058",
-  appId: "1:991096957058:web:3cdde076edfa6c6c8c2c9b",
-  measurementId: "G-71G1F87D6Y"
-};
