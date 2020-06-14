57ec16180d0ad3f67c78b323ead0bb2a53120cab
Circuandante <circuandante@gmail.com>
2020-06-13 21:04:03 -0500

    add firebase

--- diablo3/src/main.js
+++ diablo3/src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue';
+import * as firebase from 'firebase/app';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+
+import { firebaseConfig } from './api/firebase/index';
+
+
+firebase.initializeApp(firebaseConfig);
 
 Vue.config.productionTip = false;
 
